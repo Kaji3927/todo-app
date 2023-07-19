5778e239121ae2a4dfe53514476a65ed9dbfa5ac
Kaji <k.kaji3927@gmail.com>
2023-07-19 22:25:17 +0900

    create todos component

--- src/components/todos/TodoList.jsx
+++ src/components/todos/TodoList.jsx
@@ -1,7 +1,36 @@
 import styles from "@/styles/todo-list.module.scss";
 
+const todos = [
+  {
+    id: 1,
+    todo: "Learn React",
+    isComplete: false,
+  },
+  {
+    id: 2,
+    todo: "Learn Next.js",
+    isComplete: false,
+  },
+  {
+    id: 3,
+    todo: "Learn Prisma",
+    isComplete: false,
+  },
+];
+
 const TodoList = () => {
-  return <div>TodoList</div>;
+  return (
+    <div className={styles.todos}>
+      <ul className={styles.list}>
+        {todos.map((todo) => (
+          <li className={styles.todo} key={todo.id}>
+            <span className={styles.circle} />
+            <span className={styles.text}>{todo.todo}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default TodoList;
